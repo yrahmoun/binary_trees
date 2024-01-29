8aa53b6fa5e7a2eb6deccb55837a07ca3301e6e7
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-01-29 06:24:11 -0800

    task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a node is the root of the tree
+ * @node: node to check
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node || node->parent)
+		return (0);
+	return (1);
+}
